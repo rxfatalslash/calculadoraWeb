02dd75596fb473a424f0746904ff3300b0f1b001
rxfatalslash <aaronsanchezmenendez@gmail.com>
2023-03-23 20:15:43 +0100

    Escribir al pulsar teclas

--- index.js
+++ index.js
@@ -12,3 +12,61 @@ function solve() {
     document.querySelector('#result').value = y;
     return y;
 }
+
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case '1':
+            document.getElementById('1').click();
+            break;
+        case '2':
+            document.getElementById('2').click();
+            break;
+        case '3':
+            document.getElementById('3').click();
+            break;
+        case '4':
+            document.getElementById('4').click();
+            break;
+        case '5':
+            document.getElementById('5').click();
+            break;
+        case '6':
+            document.getElementById('6').click();
+            break;
+        case '7':
+            document.getElementById('7').click();
+            break;
+        case '8':
+            document.getElementById('8').click();
+            break;
+        case '9':
+            document.getElementById('9').click();
+            break;
+        case '0':
+            document.getElementById('0').click();
+            break;
+        case '/':
+            document.getElementById('/').click();
+            break;
+        case '*':
+            document.getElementById('*').click();
+            break;
+        case '-':
+            document.getElementById('-').click();
+            break;
+        case '+':
+            document.getElementById('+').click();
+            break;
+        case '.':
+            document.getElementById('.').click();
+            break;
+        case 'Enter':
+            document.getElementById('=').click();
+            break;
+        case 'Backspace':
+            document.getElementById('C').click();
+            break;
+        default:
+            return;
+    }
+})
